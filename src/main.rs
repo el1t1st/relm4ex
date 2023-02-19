096f8bf5cccec178fc698668e08c630e8fccdc97
Alex K <kleydints@petalmail.com>
2023-02-19 16:06:20 +0100

    fixed texttagtable but still bug with widgets append

--- src/main.rs
+++ src/main.rs
@@ -31,6 +31,7 @@ struct AppModel {
 	keypress_result: bool,
 	css_provider: gtk::CssProvider,
 	display: gtk::gdk::Display,
+	text_tag_table: gtk::TextTagTable,
 }
 
 #[derive(Debug)]
@@ -50,16 +51,14 @@ impl SimpleComponent for AppModel {
 	type Input = AppInput;
 	type Output = ();
 	type Init = AppModel;
-	// ApplicationWindow supports connect for listening to keypresses
 	type Root = gtk::Window;
 	type Widgets = AppWidgets;
 
 	fn init_root() -> Self::Root {
 		gtk::Window::builder()
-			.title("TextView Layered")
+			.title("TextView With Pango Colors TextTagTable")
 			.default_width(500)
 			.default_height(500)
-			// Adding a listener for key-presses
 			.build()
 	}
 
@@ -75,12 +74,14 @@ impl SimpleComponent for AppModel {
 			keypress_result: false,
 			css_provider: gtk::CssProvider::new(),
 			display: gtk::gdk::Display::default().unwrap(),
+			text_tag_table: gtk::TextTagTable::new(),
 		};
 		// Activate CSS Provider
-		model.css_provider.connect_parsing_error(|_, _, error| {
-			println!("{:?}", error);
-		});
-
+		// What does this do? And shouldn't we do this before the load_from_path?
+		// model.css_provider.connect_parsing_error(|_, _, error| {
+		// 	println!("{:?}", error);
+		// });
+		//
 		model.css_provider.load_from_path("test.css");
 		// match ret {
 		// 	Ok(ret) => println!("Worked? {:?}", ret),
@@ -95,16 +96,20 @@ impl SimpleComponent for AppModel {
 
 		// Push the initial TEXT to the base_text
 		model.base_text.set_text(TEXT);
-		model.css_provider.load_from_path("test.css");
 
-		// todo: fix this
-		let tag = model
-			.base_text
-			.tag_table()
-			.create_tag(Some("orange"), bgcolor = "orange");
-
+		// create tag table
+		model.text_tag_table = model.base_text.tag_table();
+		// create tags
+		let green_tag = gtk::TextTag::new(Some("green"));
+		green_tag.set_background(Some("green"));
+		let red_tag = gtk::TextTag::new(Some("red"));
+		red_tag.set_background(Some("red"));
+		// Add tags to tag_table
+		model.text_tag_table.add(&green_tag);
+		model.text_tag_table.add(&red_tag);
+		// Once this works we can add more tags for font, fontsize, ...
 		model.base_text.apply_tag(
-			tag,
+			&red_tag,
 			&model.base_text.start_iter(),
 			&model.base_text.end_iter(),
 		);
@@ -112,37 +117,33 @@ impl SimpleComponent for AppModel {
 		// Build the UI
 		let vbox = gtk::Box::builder()
 			.orientation(gtk::Orientation::Vertical)
-			.spacing(5)
 			.build();
 
 		let base_textview = gtk::TextView::builder()
 			// To connect the keypresses to the base_textview widget we need to set it focusable
 			.focusable(true)
 			.editable(true)
-			.height_request(10)
 			.wrap_mode(gtk::WrapMode::Word)
 			.buffer(&model.base_text)
-			// .opacity(0.3)
 			.build();
 
-		let label_keystroke = gtk::Label::builder()
-			.label("The current keystroke:")
-			.build();
-		let label_index = gtk::Label::builder().label("The current index").build();
+		let label_keystroke =
+			gtk::Label::builder().label("Current keystroke:").build();
+		let label_index = gtk::Label::builder().label("Current index").build();
 
 		window.set_child(Some(&vbox));
-		vbox.set_margin_all(5);
+		// vbox.set_margin_all(5);
 		vbox.append(&base_textview);
 		vbox.append(&label_index);
-		vbox.append(&base_textview);
+		vbox.append(&label_index);
 
 		// listen to keyevents
 		// GTK keyboard events listener
 
 		let widgets = AppWidgets {
 			base_textview,
-			label_keystroke,
 			label_index,
+			label_keystroke,
 		};
 		ComponentParts { model, widgets }
 	}
@@ -174,5 +175,7 @@ fn main() {
 		cursor_index: 0,
 		keypress_result: true,
 		css_provider: gtk::CssProvider::new(),
+		text_tag_table: gtk::TextTagTable::new(),
+		display: gtk::gdk::Display::default().unwrap(),
 	});
 }
