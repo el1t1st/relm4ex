78b3ebe7980b8ba100f1b46da4c74c81b34e29c1
Alex K <kleydints@petalmail.com>
2023-02-20 19:09:08 +0100

    added overwrite and cursor visible

--- src/main.rs
+++ src/main.rs
@@ -1,12 +1,6 @@
 use gtk::{
-	gdk::Display,
-	pango,
-	prelude::{
-		BoxExt, GtkApplicationExt, GtkWindowExt, TextBufferExt,
-		TextBufferExtManual, TextTagExt, TextViewExt,
-	},
+	prelude::{BoxExt, GtkWindowExt, TextBufferExt, TextTagExt, TextViewExt},
 	traits::WidgetExt,
-	TextTagTable,
 };
 use relm4::{
 	gtk, ComponentParts, ComponentSender, RelmApp, RelmWidgetExt, SimpleComponent,
@@ -21,16 +15,13 @@ use relm4::{
 // By using Pango it is possible to change the color of
 // characters with start_iter / end_iter and Pango tags
 
-const TEXT: &str = "Trying to solve the issue in a 
-	\n different why by using \n 
-	TextView with Pango parsing and a keypress listener!";
+const TEXT: &str = "Trying to solve the issue in a different why by using TextView with Pango parsing and a keypress listener!";
 
 struct AppModel {
 	base_text: gtk::TextBuffer,
 	cursor_index: u16,
-	keypress_result: bool,
-	css_provider: gtk::CssProvider,
-	display: gtk::gdk::Display,
+	// css_provider: gtk::CssProvider,
+	// display: gtk::gdk::Display,
 	text_tag_table: gtk::TextTagTable,
 }
 
@@ -44,8 +35,6 @@ enum AppInput {
 struct AppWidgets {
 	// We can leave the label for now
 	base_textview: gtk::TextView,
-	// label_index: gtk::Label,
-	// label_keystroke: gtk::Label,
 }
 
 impl SimpleComponent for AppModel {
@@ -64,21 +53,18 @@ impl SimpleComponent for AppModel {
 	}
 
 	fn init(
-		//
-		state: Self::Init,
+		_state: Self::Init,
 		window: &Self::Root,
 		_sender: ComponentSender<Self>,
 	) -> relm4::ComponentParts<Self> {
-		let mut model = state;
 		// Initialize the AppModel / state
-		// let mut model = AppModel {
-		// 	base_text: gtk::TextBuffer::new(None),
-		// 	cursor_index: 0,
-		// 	keypress_result: false,
-		// 	css_provider: gtk::CssProvider::new(),
-		// 	display: gtk::gdk::Display::default().unwrap(),
-		// 	text_tag_table: gtk::TextTagTable::new(),
-		// };
+		let mut model = AppModel {
+			base_text: gtk::TextBuffer::new(None),
+			cursor_index: 0,
+			// css_provider: gtk::CssProvider::new(),
+			// display: gtk::gdk::Display::default().unwrap(),
+			text_tag_table: gtk::TextTagTable::new(),
+		};
 		// Activate CSS Provider
 		// What does this do? And shouldn't we do this before the load_from_path?
 		// model.css_provider.connect_parsing_error(|_, _, error| {
@@ -99,22 +85,29 @@ impl SimpleComponent for AppModel {
 		// 	gtk::STYLE_PROVIDER_PRIORITY_APPLICATION,
 		// );
 
+		model.base_text.set_text(TEXT);
+
 		// create tag table
 		model.text_tag_table = model.base_text.tag_table();
 		// create tags
 		let green_tag = gtk::TextTag::new(Some("green"));
 		green_tag.set_background(Some("green"));
+
 		let red_tag = gtk::TextTag::new(Some("red"));
 		red_tag.set_background(Some("red"));
+
 		// Add tags to tag_table
 		model.text_tag_table.add(&green_tag);
 		model.text_tag_table.add(&red_tag);
+
 		// Once this works we can add more tags for font, fontsize, ...
 		model.base_text.apply_tag(
 			&red_tag,
 			&model.base_text.start_iter(),
 			&model.base_text.end_iter(),
 		);
+
+		model.base_text.place_cursor(&model.base_text.start_iter());
 
 		// Build the UI
 		let vbox = gtk::Box::builder()
@@ -125,22 +118,18 @@ impl SimpleComponent for AppModel {
 		let base_textview = gtk::TextView::builder()
 			.focusable(true)
 			.editable(true)
-			.wrap_mode(gtk::WrapMode::Word)
+			.overwrite(true) // The cursor needs to overwrite
+			.cursor_visible(true)
+			.wrap_mode(gtk::WrapMode::WordChar)
 			.buffer(&model.base_text)
 			.build();
-
-		model.base_text.set_text(TEXT);
-
-		let label_keystroke =
-			gtk::Label::builder().label("Current keystroke:").build();
-		let label_index = gtk::Label::builder().label("Current index").build();
 
 		window.set_child(Some(&vbox));
 		vbox.set_margin_all(10);
 		vbox.append(&base_textview);
-		vbox.append(&label_index);
-		vbox.append(&label_index);
 
+		// get the bounds
+		// set the character to the first char
 		// listen to keyevents
 		// GTK keyboard events listener
 
@@ -148,15 +137,22 @@ impl SimpleComponent for AppModel {
 		ComponentParts { model, widgets }
 	}
 
-	fn update(&mut self, _message: Self::Input, _sender: ComponentSender<Self>) {
-		// match message {
-		// 	AppInput::Increment => {
-		// 		self.counter = self.counter.wrapping_add(1);
-		// 	},
-		// 	AppInput::Decrement => {
-		// 		self.counter = self.counter.wrapping_sub(1);
-		// 	},
-		// }
+	fn update(&mut self, message: Self::Input, _sender: ComponentSender<Self>) {
+		match message {
+			AppInput::CheckChar => {
+				// get the &model.current_index
+				// take the string[current_index]
+				// get current_keystroke
+				// if == add_tag green else add_tag_red
+				// model.current_index.wrapping_add(1);
+				println!("Checking character");
+			},
+			AppInput::BackOneChar => {
+				// remove tag from the character with current_index -1
+				// &model.current_index.wrapping_sub(1);
+				println!("One character back");
+			},
+		}
 	}
 
 	fn update_view(
@@ -165,6 +161,7 @@ impl SimpleComponent for AppModel {
 		_sender: ComponentSender<Self>,
 	) {
 		// Update the view
+		// the model.text_base needs to update but ...?
 	}
 }
 
@@ -173,9 +170,8 @@ fn main() {
 	app.run::<AppModel>(AppModel {
 		base_text: gtk::TextBuffer::new(None),
 		cursor_index: 0,
-		keypress_result: true,
-		css_provider: gtk::CssProvider::new(),
+		// css_provider: gtk::CssProvider::new(),
 		text_tag_table: gtk::TextTagTable::new(),
-		display: gtk::gdk::Display::default().unwrap(),
+		// display: gtk::gdk::Display::default().unwrap(),
 	});
 }
